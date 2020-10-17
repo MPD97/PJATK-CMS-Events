60f516a1fc9cc2144152696cff76c8dd965d9116
MPD97 <mateuszpracadom@gmail.com>
2020-10-17 23:49:22 +0200

    Entities

--- WebApplication1/Entities/AppIdentityUser.cs
+++ WebApplication1/Entities/AppIdentityUser.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace CMS.Entities
 {
-    public class AppIdentityUser : IdentityUser
+    public class AppIdentityUser : IdentityUser<int>
     {
 
     }
@@ -19,21 +19,80 @@ namespace CMS.Entities
 
 
         [MaxLength(250)]
-        public string Name { get; set; }
+        public string NamePolish { get; set; }
+
+        [MaxLength(250)]
+        public string NameEnglish { get; set; }
+
+        public decimal Lat { get; set; }
+        public decimal Lon { get; set; }
+
         public DateTimeOffset Date { get; set; }
 
 
+        public virtual ICollection<Ticket> Tickets { get; set; }
     }
     public class Ticket
     {
         [Column("ID")]
         public int TicketId { get; set; }
 
-        public decimal PricePLN { get; set; }
-
+        public decimal? PricePLN { get; set; }
 
         public int EventId { get; set; }
-        public Event Event { get; set; }
+        public virtual Event Event { get; set; }
 
+        public int PurchaseId { get; set; }
+        public virtual Purchase Purchase { get; set; }
+    }
+    public class Purchase
+    {
+        [Column("ID")]
+        public int PurchaseId { get; set; }
+
+        public PurchaseType PurchaseType { get; set; }
+
+        public int TicketId { get; set; }
+        public virtual Ticket Ticket { get; set; }
+
+        public int AppIdentityUserId { get; set; }
+        public virtual AppIdentityUser AppIdentityUser { get; set; }
+
+        public int PersonId { get; set; }
+        public virtual Person Person { get; set; }
+    }
+    public class Person
+    {
+        [Column("ID")]
+        public int PersonId { get; set; }
+
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [MaxLength(100)]
+        public string SecondName { get; set; }
+
+        [MaxLength(150)]
+        public string LastName { get; set; }
+
+        public byte Age { get; set; }
+
+        [MaxLength(100)]
+        public string City { get; set; }
+
+        [MaxLength(150)]
+        public string Street { get; set; }
+
+        [MaxLength(20)]
+        public string StreetNumber { get; set; }
+
+        [MaxLength(8)]
+        public string ZipCode { get; set; }
+
+    }
+    public enum PurchaseType
+    {
+        Reservation,
+        Completed
     }
 }
