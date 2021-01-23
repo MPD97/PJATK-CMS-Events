b3a1a3764242ea33418915b9af564a4d005c6ee6
MPD97 <mateuszpracadom@gmail.com>
2021-01-23 17:46:17 +0100

    Cart as mvc

--- src/CMS.Web/CMS.Web/Controllers/CartController.cs
+++ src/CMS.Web/CMS.Web/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using CMS.Core.Entites;
+using CMS.Web.Models;
 using CMS.Web.Requests;
 using CMS.Web.Services;
 using Microsoft.AspNetCore.Authorization;
@@ -15,10 +16,23 @@ namespace CMS.Web.Controllers
     [Authorize]
     public class CartController : Controller
     {
+        private readonly IEventService _service;
+
+        public CartController(IEventService service)
+        {
+            _service = service;
+        }
+
         [HttpGet("/Cart/checkout")]
         public async Task<IActionResult> Submit()
         {
             return View();
+        }
+
+        [HttpGet("/Cart/cart")]
+        public async Task<IActionResult> Cart()
+        {
+            return View(new CartModel() { Events = _service.GetEvents().Result });
         }
     }
 }
