60060b37afc7dfb7950d32da62b6fb3bb7855a73
MPD97 <mateuszpracadom@gmail.com>
2021-01-23 17:30:23 +0100

    CartController

---
+++ src/CMS.Web/CMS.Web/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CMS.Core.Entites;
+using CMS.Web.Requests;
+using CMS.Web.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+namespace CMS.Web.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        [HttpGet("/Cart/checkout")]
+        public async Task<IActionResult> Submit()
+        {
+            return View();
+        }
+    }
+}
