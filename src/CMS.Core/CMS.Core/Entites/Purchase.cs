3362cc728f05deda327a4f4f054a31631f6af21f
Piotr Wasilewski <61659817+S17918@users.noreply.github.com>
2021-01-28 21:57:51 +0100

    MyEvents FrontEnd + Translations

--- src/CMS.Core/CMS.Core/Entites/Purchase.cs
+++ src/CMS.Core/CMS.Core/Entites/Purchase.cs
@@ -6,6 +6,7 @@ namespace CMS.Core.Entites
     public class Purchase : BaseEntity
     {
         public int UserId { get; set; }
+
         public virtual ApplicationUser User { get; set; }
 
         public virtual ICollection<Ticket> Tickets { get; set; }
